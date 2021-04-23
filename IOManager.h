dfe6b2b82083ba9c76924b62aedcb36b7905272c
Omer Gery <omergery1996@gmail.com>
2021-04-23 18:13:01 +0300

    deleted Operator=__copyctor

---
+++ IOManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "AdjacencyListGraph.h"
+#include "AbstractGraph.h"
+#include "ConstTypes.h"
+#include "GraphAlgorithms.h"
+#include "DynamicArray.h"
+#include "AdjacencyMatrixGraph.h"
+#include "minHeap.h"
+#include "AbstractPriorityQueue.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+namespace AlgoGraph
+{
+	bool CheckInputFileValidity(string i_inputFileName, int& endLocation);
+	bool firstThreeSelectionAreValid(int numberOfVertex, int pathStartingVertex, int pathEndVertex);
+	bool CheckComandArguments(int argc);
+	void PrintWrongInput();
+	void RunAlgorithms(AdjancencyListGraph& ListGraph, AdjacencyMatrixGraph& MatrixGraph,int pathStartingVertex,int pathEndVertex);
+}
